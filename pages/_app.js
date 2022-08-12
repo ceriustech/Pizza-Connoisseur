e2d2c8d8f7309f297cde3bbb92cc5997570e1c18
Bill <ceriustech@gmail.com>
2022-08-11 21:25:12 -0500

    feat(favicon): Added new favicon images

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,9 @@
-import Head from 'next/head';
 import Footer from './footer';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
-			<Head>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
 			<Component {...pageProps} />
 			<Footer />
 		</>
--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,24 @@ const Home = () => {
 		<div className={styles.container}>
 			<Head>
 				<title>Pizza Picker</title>
-        <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
-        <link rel="manifest" href="/static/site.webmanifest" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/static/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/static/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/static/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/static/site.webmanifest" />
 				<link rel="icon" href="/static/favicon.ico" />
 			</Head>
 			<main className={styles.main}>
