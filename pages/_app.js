d1d1f152d207352af380d4e624b765415791b2f2
Bill <ceriustech@gmail.com>
2022-08-11 20:55:21 -0500

    feat: Added next document and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
-import '../styles/globals.css';
+import Head from 'next/head';
 import Footer from './footer';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
+			<Head>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 			<Component {...pageProps} />
 			<Footer />
 		</>
--- pages/footer.js
+++ pages/footer.js
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import styles from '../styles/Footer.module.css';
 
 const Footer = () => <footer className={styles.footer}></footer>;
