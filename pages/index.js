e73641b2d0c3de6d8e1ed6abb49b1829a5603d32
Bill <ceriustech@gmail.com>
2022-10-02 18:46:14 -0500

    feat: Added state to conditionally to render out card layout and heading

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Banner from '../components/Banner/Banner';
 import StateSearch from '../components/Search/StateSearch/StateSearch';
@@ -24,9 +24,11 @@ const Home = ({ pizzaShops }) => {
 	const { trackLocationHandler, latLong, locationErrMsg, isFindingLocation } =
 		useTrackLocation();
 
+	const [pizzaPlaces, setPizzaPlaces] = useState('');
+
 	console.log('%cLOCATION DATA', 'font-size: 1.5em; color:red');
 	console.log({ latLong, locationErrMsg });
-	console.log('PIZZA SHOPS', pizzaShops);
+	console.log('PIZZA PLACES', pizzaPlaces);
 
 	useEffect(() => {
 		async function getPizzaRestaurantsByLocation() {
@@ -36,8 +38,9 @@ const Home = ({ pizzaShops }) => {
 						latLong,
 						15
 					);
+					setPizzaPlaces(fetchedPizzaRestaurants);
 					console.log('%cFETCHED PIZZA SHOPS:', 'font-size:1.5em;color:yellow');
-					console.log(fetchedPizzaRestaurants);
+					console.log(pizzaPlaces);
 				} catch (error) {
 					console.log('UNABLE TO FETCH RESTAURANT DATA:', error);
 				}
@@ -53,6 +56,24 @@ const Home = ({ pizzaShops }) => {
 	};
 
 	const { thumbUp } = icons;
+
+	const pizzaPlaceCount = pizzaPlaces.length;
+
+	let pizzaRestaurantsHeader;
+
+	if (pizzaPlaceCount > 0) {
+		pizzaRestaurantsHeader = (
+			<h2 className={styles.heading}>
+				<span>Pizza</span> Restaurants Near You
+			</h2>
+		);
+	} else {
+		pizzaRestaurantsHeader = (
+			<h2 className={styles.heading}>
+				<span>Checkout</span> These Restaurants
+			</h2>
+		);
+	}
 
 	return (
 		<div className={styles.container}>
@@ -94,9 +115,7 @@ const Home = ({ pizzaShops }) => {
 						<CitySearch />
 					</div>
 					<div className={styles.headingContainer}>
-						<h2 className={styles.heading}>
-							<span>Top</span> Rated Pizza Shops
-						</h2>
+						{pizzaRestaurantsHeader}
 					</div>
 					{locationErrMsg && (
 						<div className={styles.errorMsgContainer}>
@@ -106,6 +125,24 @@ const Home = ({ pizzaShops }) => {
 				</section>
 
 				<section className="restarurantGrid">
+					{pizzaPlaceCount > 0 && (
+						<>
+							<div className={styles.cardLayout}>
+								{pizzaPlaces.map((shop) => (
+									<Card
+										key={shop.id}
+										name={shop.name}
+										href={`/pizza-shop/${shop.id}`}
+										imgUrl={shop?.imgUrl || pizzaShopsData[0].imgUrl}
+										upVoteImgUrl={thumbUp}
+										city={shop.city}
+										state={shop.state}
+									/>
+								))}
+							</div>
+						</>
+					)}
+
 					{(pizzaShops.length > 0 && (
 						<>
 							<div className={styles.cardLayout}>
